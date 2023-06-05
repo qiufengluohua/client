5e467ed688d25c8703c640bb2a871247619696aa
chenshenjun <353668108@qq.com>
2023-06-05 18:18:10 +0800

    push commit

--- src/main/java/com/itheima/App.java
+++ src/main/java/com/itheima/App.java
@@ -31,5 +31,6 @@ public class App implements WebMvcConfigurer {
         System.out.println("hello git8");
         System.out.println("hello git9");
         System.out.println("hello git10");
+        System.out.println("hello git10");
     }
 }
